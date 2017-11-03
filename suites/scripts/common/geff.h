f2ba365c59cb3662bb178969c3e7404aebbbf9ff
Blazej Krzeminski <Blazej.Krzeminski@ecmwf.int>
2017-11-03 12:58:50 +0000

    improved setting up an experiment

--- suites/scripts/common/geff.h
+++ suites/scripts/common/geff.h
@@ -1,2 +1,6 @@
 # version of the GEFF system
 geff_version=2.0
+
+# if expver not specified, use $geff_version
+# as an identifier of this run in the archive.
+expver=<?config.get('expver', '$geff_version')?>
