91b6fca082104433a194201da08e9a61d69e411c
Blazej Krzeminski <Blazej.Krzeminski@ecmwf.int>
2018-07-25 19:18:00 +0100

    use experiment version instead of model version in archive paths

--- suites/scripts/common/geff.h
+++ suites/scripts/common/geff.h
@@ -1,6 +1,2 @@
 # version of the GEFF system
 geff_version=2.2
-
-# if expver not specified, use $geff_version
-# as an identifier of this run in the archive.
-expver=<?config.get('expver', '$geff_version')?>
