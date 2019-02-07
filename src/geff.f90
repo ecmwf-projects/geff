1b1d5e7d03fce6868ed3fc17c0c7e920f82c7942
Francesca Di Giuseppe <Francesca.DiGiuseppe@ecmwf.int>
2019-02-07 12:22:25 +0000

    lsm back to 0.0001

--- src/geff.f90
+++ src/geff.f90
@@ -250,7 +250,7 @@ PROGRAM geff
 
            ! calculations is performed only on land points for all the indices
            !
-      IF (rlsm(ix,iy) .gt. 0.08 .and. zlat .gt. -60.0 )  THEN      
+      IF (rlsm(ix,iy) .gt. 0.0001 .and. zlat .gt. -60.0 )  THEN      
             ! 0- set-up conditions 
    !---------------------------------------------------------------------------
     
