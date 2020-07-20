5631b0b56d4f80acfa51f03bb60f17a9289e2c03
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-07-20 22:32:38 +0100

    Cleanup (fprettify -i 4 -w 3), minor edits

--- src/mo_nfdrs.f90
+++ src/mo_nfdrs.f90
@@ -12,37 +12,37 @@
 MODULE mo_nfdrs
 
     USE mo_constants
-    USE mo_utilities
+    USE mo_utilities, only: kelvin_to_fahrenheit
 
     IMPLICIT NONE
 
     ! prognostic
     ! NOTE: first size members are the same as fuelweight_type
     TYPE mc_type
-        REAL :: r1hr    = rfillvalue
-        REAL :: r10hr   = rfillvalue
-        REAL :: r100hr  = rfillvalue
+        REAL :: r1hr = rfillvalue
+        REAL :: r10hr = rfillvalue
+        REAL :: r100hr = rfillvalue
         REAL :: r1000hr = rfillvalue
-        REAL :: rherb   = rfillvalue
-        REAL :: rwood   = rfillvalue
-        REAL :: rx1000  = rfillvalue
+        REAL :: rherb = rfillvalue
+        REAL :: rwood = rfillvalue
+        REAL :: rx1000 = rfillvalue
         REAL :: rbndryt = rfillvalue
-     END TYPE mc_type
+    END TYPE
 
     ! diagnostic
     TYPE fire_prop_type
-        REAL :: ros     = rfillvalue  !< rate of spread
-        INTEGER :: sc   = ifillvalue  !< spread component
-        INTEGER :: erc  = ifillvalue  !< energy release component
-        INTEGER :: bi   = ifillvalue  !< burning index
-     END TYPE fire_prop_type
+        REAL :: ros = rfillvalue  !< rate of spread
+        INTEGER :: sc = ifillvalue  !< spread component
+        INTEGER :: erc = ifillvalue  !< energy release component
+        INTEGER :: bi = ifillvalue  !< burning index
+    END TYPE
 
     TYPE fire_prob_type
-        INTEGER :: ic   = ifillvalue  !< ignition probability
+        INTEGER :: ic = ifillvalue  !< ignition probability
         INTEGER :: mcoi = ifillvalue  !< human caused fire occurrence
-        INTEGER :: loi  = ifillvalue  !< lightining coused occurrence index
-        REAL :: fli     = rfillvalue  !< fire load index
-     END TYPE fire_prob_type
+        INTEGER :: loi = ifillvalue  !< lightining coused occurrence index
+        REAL :: fli = rfillvalue  !< fire load index
+    END TYPE
 
     TYPE(mc_type), ALLOCATABLE :: mc(:)
     TYPE(fire_prop_type), ALLOCATABLE :: fire_prop(:)
@@ -50,56 +50,48 @@ MODULE mo_nfdrs
 
 CONTAINS
 
-SUBROUTINE emc(tempfh, rh, remc)
-    REAL, INTENT(IN)  :: tempfh  !< temperature [Fahrenheit]
-    REAL, INTENT(IN)  :: rh      !< relative humidity [%]
-    REAL, INTENT(OUT) :: remc
+    SUBROUTINE emc(tempfh, rh, remc)
+        REAL, INTENT(IN) :: tempfh  !< temperature [Fahrenheit]
+        REAL, INTENT(IN) :: rh      !< relative humidity [%]
+        REAL, INTENT(OUT) :: remc
 
-  IF ( rh .lt. 10) THEN
-     remc=0.03229+0.281073*rh-0.000578*tempfh*rh
-  ELSE IF ( rh .lt. 50) THEN
-     remc=2.22749+0.160107*rh-0.014784*tempfh
-  ELSE
-     remc=21.0606+0.005565*rh**2-0.00035*rh*tempfh-0.483199*rh
-  ENDIF
-END SUBROUTINE emc
+        IF (rh .lt. 10) THEN
+            remc = 0.03229 + 0.281073 * rh - 0.000578 * tempfh * rh
+        ELSE IF (rh .lt. 50) THEN
+            remc = 2.22749 + 0.160107 * rh - 0.014784 * tempfh
+        ELSE
+            remc = 21.0606 + 0.005565 * rh**2 - 0.00035 * rh * tempfh - 0.483199 * rh
+        END IF
+    END SUBROUTINE emc
 
-SUBROUTINE kwet(mc1000,increment,rkwet)
+    SUBROUTINE kwet(mc1000, increment, rkwet)
+        REAL, INTENT(IN) :: mc1000, increment
+        REAL, INTENT(OUT) :: rkwet
 
-  REAL, INTENT(IN)    :: mc1000,increment
-  REAL, INTENT(OUT) :: rkwet
+        ! the kwet factor just depends on the slow evolving moisture content
+        IF (increment .le. 0 .or. mc1000 .gt. 25) THEN
+            rkwet = 1.0
+        ELSE IF (mc1000 .gt. 10) THEN
+            rkwet = (0.0333 * mc1000 + 0.1675)
+        ELSE
+            rkwet = 0.5
+        END IF
+    END SUBROUTINE kwet
 
- ! the kwet factor just depends on the slow evolving moisture content
-  !
+    SUBROUTINE ktmp(tmin, tmax, rktmp)
+        REAL, INTENT(IN) :: tmin, tmax
+        REAL, INTENT(OUT) :: rktmp
 
- IF (mc1000 .gt. 25 )THEN
-    rkwet=1.0
- ELSE IF (mc1000 .gt. 10)THEN
-    rkwet=(0.0333*mc1000 +0.1675)
- ELSE
-    rkwet=0.5
- END IF
- !overwrite the above if the increment in moisture content is negative
+        !local
+        REAL :: tmaxfh, tminfh
+        tminfh = kelvin_to_fahrenheit(tmin)
+        tmaxfh = kelvin_to_fahrenheit(tmax)
 
- IF (increment  .LE.  0) rkwet=1.0
-END SUBROUTINE kwet
-
-
-SUBROUTINE ktmp(tmin,tmax,rktmp)
-
-  REAL, INTENT(IN)    :: tmin,tmax
-  REAL, INTENT(OUT)   :: rktmp
-
-  !local
-  !
-  REAL :: tmaxfh,tminfh
- tminfh=kelvin_to_fahrenheit(tmin)
- tmaxfh=kelvin_to_fahrenheit(tmax)
-
- rktmp=1.0
-
- IF ( ((tminfh+tmaxfh)*0.5) <= 50 ) rktmp=0.6
-END SUBROUTINE ktmp
-
+        IF (((tminfh + tmaxfh) * 0.5) <= 50) THEN
+            rktmp = 0.6
+        ELSE
+            rktmp = 1.0
+        END IF
+    END SUBROUTINE ktmp
 
 END MODULE mo_nfdrs
